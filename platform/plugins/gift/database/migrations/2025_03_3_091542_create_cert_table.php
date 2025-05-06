dbcc65dd4060211c57caf7b8ac48283809455508
ibrahimaah <ibrahim.a.a.h.2017@gmail.com>
2025-05-06 13:41:20 +0300

    adding !Schema::hasTable to migrations

--- platform/plugins/gift/database/migrations/2025_03_3_091542_create_cert_table.php
+++ platform/plugins/gift/database/migrations/2025_03_3_091542_create_cert_table.php
@@ -6,22 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
-    {
-        Schema::dropIfExists('certs');
-
-        Schema::create('certs', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('font_size');
-            $table->string('font_color');
-            $table->string('status', 60)->default('published');
-            $table->string('image')->nullable();
-            $table->integer('from_x'); // X-coordinate start
-            $table->integer('from_y'); // Y-coordinate start
-            $table->integer('to_x');   // X-coordinate end
-            $table->integer('to_y');   // Y-coordinate end
-            $table->timestamps();
-        });
+    { 
+        if (!Schema::hasTable('certs')) 
+        {
+            Schema::create('certs', function (Blueprint $table) 
+            {
+                $table->id();
+                $table->string('name');
+                $table->string('font_size');
+                $table->string('font_color');
+                $table->string('status', 60)->default('published');
+                $table->string('image')->nullable();
+                $table->integer('from_x'); // X-coordinate start
+                $table->integer('from_y'); // Y-coordinate start
+                $table->integer('to_x');   // X-coordinate end
+                $table->integer('to_y');   // Y-coordinate end
+                $table->timestamps();
+            });
+        }
 
     }
 
